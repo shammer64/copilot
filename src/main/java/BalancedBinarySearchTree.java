a100c8474936dd6a5d5266d85149e0e08a2a7708
Scott Hammer <shammer64@gmail.com>
2024-02-09 16:12:41 -0500

    Enhancements to add() and implementations of depth() and isBalanced()

--- src/main/java/BalancedBinarySearchTree.java
+++ src/main/java/BalancedBinarySearchTree.java
@@ -1,4 +1,4 @@
-public class BalancedBinarySearchTree<T> {
+public class BalancedBinarySearchTree<T extends Comparable<T>> {
     private BinaryTreeNode<T> root;
 
     public boolean isEmpty() {
@@ -6,18 +6,67 @@ public class BalancedBinarySearchTree<T> {
     }
 
     public void add(T newItem) {
-        root = new BinaryTreeNode<>(newItem);
+        if (isEmpty()) root = new BinaryTreeNode<>(newItem);
+        else root.add(newItem);
     }
 
     public int size() {
-        return isEmpty() ? 0 : 1;
+        return isEmpty() ? 0 : root.size();
     }
 
-    private static class BinaryTreeNode<T> {
+    public boolean isBalanced() {
+        if (isEmpty()) return true;
+        return root.isBalanced();
+    }
+
+    public int depth() {
+        return isEmpty()? 0 : root.depth();
+    }
+
+    private static class BinaryTreeNode<T extends Comparable<T>> {
         private final T item;
+        private BinaryTreeNode<T> left;
+        private BinaryTreeNode<T> right;
 
         public BinaryTreeNode(T item) {
             this.item = item;
+        }
+
+        public int size() {
+            return 1 +
+                    (left == null ? 0 : left.size()) +
+                    (right == null ? 0 : right.size());
+        }
+
+        public void add(T newItem) {
+            if (item.compareTo(newItem) <= 0)
+                addLeft(newItem);
+            else
+                addRight(newItem);
+        }
+
+        private void addRight(T newItem) {
+            if (right == null)
+                right = new BinaryTreeNode<>(newItem);
+            else
+                right.add(newItem);
+        }
+
+        private void addLeft(T newItem) {
+            if (left == null)
+                left = new BinaryTreeNode<>(newItem);
+            else
+                left.add(newItem);
+        }
+
+        public int depth() {
+            return 1 + Math.max(left == null ? 0 : left.depth(),
+                    right == null ? 0 : right.depth());
+        }
+
+        public boolean isBalanced() {
+            return Math.abs((left == null ? 0 : left.depth()) -
+                    (right == null ? 0 : right.depth())) <= 1;
         }
     }
 }
--- src/test/java/BalancedBinaryTreeTest.java
+++ src/test/java/BalancedBinaryTreeTest.java
@@ -29,4 +29,102 @@ public class BalancedBinaryTreeTest {
         tree.add(5);
         assertEquals(1, tree.size());
     }
+
+    @Test
+    public void twoElementTreeHasSize2() {
+        BalancedBinarySearchTree<Integer> tree = new BalancedBinarySearchTree<>();
+        tree.add(1);
+        tree.add(5);
+        assertEquals(2, tree.size());
+    }
+
+    @Test
+    public void threeElementTreeHasSize3() {
+        BalancedBinarySearchTree<Integer> tree = new BalancedBinarySearchTree<>();
+        tree.add(5);
+        tree.add(1);
+        tree.add(8);
+        assertEquals(3, tree.size());
+    }
+
+    @Test
+    public void fourElementTreeHasSize4_1() {
+        BalancedBinarySearchTree<Integer> tree = new BalancedBinarySearchTree<>();
+        tree.add(5);
+        tree.add(3);
+        tree.add(7);
+        tree.add(1);
+        assertEquals(4, tree.size());
+    }
+
+    @Test
+    public void fourElementTreeHasSize_2() {
+        BalancedBinarySearchTree<Integer> tree = new BalancedBinarySearchTree<>();
+        tree.add(5);
+        tree.add(3);
+        tree.add(7);
+        tree.add(9);
+        assertEquals(4, tree.size());
+    }
+
+    @Test
+    public void emptyTreeHasDepth0() {
+        BalancedBinarySearchTree<Integer> tree = new BalancedBinarySearchTree<>();
+        assertEquals(0, tree.depth());
+    }
+
+    @Test
+    public void singleElementTreeHasDepth1() {
+        BalancedBinarySearchTree<Integer> tree = new BalancedBinarySearchTree<>();
+        tree.add(7);
+        assertEquals(1, tree.depth());
+    }
+
+    @Test
+    public void twoElementTreeHasDepth2_1() {
+        BalancedBinarySearchTree<Integer> tree = new BalancedBinarySearchTree<>();
+        tree.add(5);
+        tree.add(7);
+        assertEquals(2, tree.depth());
+    }
+
+    @Test
+    public void twoElementTreeHasDepth2_2() {
+        BalancedBinarySearchTree<Integer> tree = new BalancedBinarySearchTree<>();
+        tree.add(7);
+        tree.add(5);
+        assertEquals(2, tree.depth());
+    }
+
+    @Test
+    public void emptyTreeIsBalanced() {
+        BalancedBinarySearchTree<Integer> tree = new BalancedBinarySearchTree<>();
+        assertTrue(tree.isBalanced());
+    }
+
+    @Test
+    public void singleElementTreeIsBalanced() {
+        BalancedBinarySearchTree<Integer> tree = new BalancedBinarySearchTree<>();
+        tree.add(5);
+        assertTrue(tree.isBalanced());
+    }
+
+//    @Test
+//    public void threeElementTreeShouldBeBalanced_1() {
+//        BalancedBinarySearchTree<Integer> tree = new BalancedBinarySearchTree<>();
+//        tree.add(1);
+//        tree.add(2);
+//        tree.add(3);
+//        assertTrue(tree.isBalanced());
+//    }
+
+//    @Test
+//    public void threeElementTreeIsUnbalanced_2() {
+//        BalancedBinarySearchTree<Integer> tree = new BalancedBinarySearchTree<>();
+//        tree.add(3);
+//        tree.add(2);
+//        tree.add(1);
+//        assertFalse(tree.isBalanced());
+//    }
+
 }
